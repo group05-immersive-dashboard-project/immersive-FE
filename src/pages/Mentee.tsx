4c18b524c01c138e2362dd81cc9e97794637ee45
vitrenSilitonga <vitrensilitonga123@gmail.com>
2023-06-08 07:43:57 +0700

    menambah login yang sudah terhubung ke api dan menambah fitur user list

--- src/pages/Mentee.tsx
+++ src/pages/Mentee.tsx
@@ -4,9 +4,35 @@ const Mentee = () => {
   return (
     <div>
       <MenuSidebar title="Dashboard" name="JohnDoe">
-        <p className="text-2xl font-medium">Chart Mantee</p>
-        <div className="">
-          <p className="text-black">halaman Mentee List</p>
+        <div className="overflow-x-auto">
+          <table className="table text-black">
+            {/* head */}
+            <thead className="bg-sky-200 text-black">
+              <tr>
+                <th>No</th>
+                <th>Full Name</th>
+                <th>email</th>
+                <th>Team </th>
+                <th>Role</th>
+                <th>Status</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr className="">
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+                <th>1</th>
+                <td>Ganderton</td>
+                <td>Control Specialist</td>
+                <td>Cy Ganderton</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </MenuSidebar>
     </div>
